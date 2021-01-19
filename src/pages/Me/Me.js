95a944f4dbc5dc96f42a5694f80b8a1500f49bd2
Zachary Binx <72980919+ZacharyBinx@users.noreply.github.com>
2021-01-19 00:29:52 -0700

    it works now

--- src/pages/Me/Me.js
+++ src/pages/Me/Me.js
@@ -6,30 +6,32 @@ import { ProfileLink } from './styles';
 const Me = ({ user }) => {
   return (
     <Layout user={user}>
-      <div>
-        <SectionTitle style={{fontFamily: 'Yusei Magic'}}>About Me</SectionTitle>
-        <Paragraph  style={{fontFamily: 'Josefin Sans'}}>{user.basics.summary}</Paragraph>
-      </div>
-      <div>
-        <SectionTitle style={{fontFamily: 'Yusei Magic'}}>Skills</SectionTitle>
+      <div className="list-margin">
         <div>
-          {user.skills.map(skill => (
-            <Pill key={skill.name}>{skill.name}</Pill>
-          ))}
+          <SectionTitle style={{ fontFamily: 'Yusei Magic' }}>About Me</SectionTitle>
+          <Paragraph style={{ fontFamily: 'Josefin Sans' }}>{user.basics.summary}</Paragraph>
         </div>
-      </div>
-      <div>
-        <SectionTitle style={{fontFamily: 'Yusei Magic'}}>Profiles</SectionTitle>
-        <ul>
-          {user.basics.profiles.map((profile, i) => (
-            <ProfileLink style={{fontFamily: 'Josefin Sans'}} key={profile.network}>
-              {i !== 0 && ' | '}
-              <a href={profile.url} target="_blank" rel="noreferrer noopener">
-                {profile.network}
-              </a>
-            </ProfileLink>
-          ))}
-        </ul>
+        <div>
+          <SectionTitle style={{ fontFamily: 'Yusei Magic' }}>Skills</SectionTitle>
+          <div>
+            {user.skills.map(skill => (
+              <Pill key={skill.name}>{skill.name}</Pill>
+            ))}
+          </div>
+        </div>
+        <div>
+          <SectionTitle style={{ fontFamily: 'Yusei Magic' }}>Profiles</SectionTitle>
+          <ul>
+            {user.basics.profiles.map((profile, i) => (
+              <ProfileLink style={{ fontFamily: 'Josefin Sans' }} key={profile.network}>
+                {i !== 0 && ' | '}
+                <a href={profile.url} target="_blank" rel="noreferrer noopener">
+                  {profile.network}
+                </a>
+              </ProfileLink>
+            ))}
+          </ul>
+        </div>
       </div>
     </Layout>
   );
